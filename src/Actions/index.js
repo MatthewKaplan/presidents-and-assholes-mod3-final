4b8d3cfb32bf39b11e1c9e59846c342b581d3632
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-06-03 09:50:59 -0600

    Add isLoading and hasErrored to store

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -2,3 +2,13 @@ export const allPresidents = presidents => ({
   type: "ALL_PRESIDENTS",
   presidents
 });
+
+export const isLoading = loading => ({
+  type: "IS_LOADING",
+  loading
+})
+
+export const hasErrored = error => ({
+  type: "HAS_ERRORED",
+  error
+})
--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from "redux";
 import presidentsReducer from "./presidentsReducer";
+import isLoadingReducer from "./isLoadingReducer";
+import hasErroredReducer from "./hasErroredReducer";
 
 
 export const rootReducer = combineReducers({
-  presidents: presidentsReducer
+  presidents: presidentsReducer,
+  loading: isLoadingReducer,
+  error: hasErroredReducer
 });
 
 export default rootReducer;
