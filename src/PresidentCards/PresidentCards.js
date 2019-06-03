808752bd09d01ec6bd346b30cf7e25f32e92ebb8
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-06-03 10:11:37 -0600

    Render presidents data to DOM

--- src/PresidentCards/PresidentCards.js
+++ src/PresidentCards/PresidentCards.js
@@ -3,9 +3,16 @@ import './PresidentCards.scss';
 
 class PresidentCards extends Component {
   render() {
+    const {presidents} = this.props;
     return (
       <div className="president-cards">
-        <h1>PRESIDENT CARDS</h1>
+        <p>Number: {presidents.number}</p>
+        <p>Name: {presidents.president}</p>
+        <p>Born: {presidents.birth_year}</p>
+        <p>Died: {presidents.death_year}</p>
+        <p>Took office: {presidents.took_office}</p>
+        <p>Left office: {presidents.left_office}</p>
+        <p>Party: {presidents.party}</p>
       </div>
     )
   }
